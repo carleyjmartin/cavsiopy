cf1344c49435d5eb73aa8276598c8689da9927a5
carleyjmartin <carleymartin@hotmail.com>
2023-03-08 12:32:05 -0600

    test6

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ release = '0.9.6'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['sphinx.ext.autodoc']
-autodoc_mock_imports = ["cartopy", "pysofa", "astropy", "geopy"]
+autodoc_mock_imports = ["cartopy", "pysofa", "astropy", "geopy", "cavsiopy"]
 
 templates_path = ['_templates']
 exclude_patterns = []
